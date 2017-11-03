8c4a94e6938fff44796ec858b1f410a1e522adb9
NS <nonstopacademy@gmail.com>
2017-11-03 09:57:42 +0800

    pagination

--- list-member.js
+++ list-member.js
@@ -1,31 +1,59 @@
 var jsf = require('jsonfile')
 var fs = require('fs')
+var chunk = require('chunk')
 var filename = 'members.json'
 
 $(function(){
   make_table()
 })
 
-function make_table()
+function make_table(page_number)
 {
-  let members = jsf.readFileSync(filename);
+  let members = jsf.readFileSync(filename)
 
-  $('#members-table tbody').html('')
-  for (var i in members) {
 
-    let member_row_html = `<tr>
-      <td>${members[i]['member-number']}</td>
-      <td>${members[i]['first-name']}</td>
-      <td>${members[i]['last-name']}</td>
-      <td>${members[i]['address01']} <br> ${members[i]['address02']} <br> ${members[i]['address03']} </td>
-      <td>${members[i]['sex']}</td>
-      <td>${members[i]['dob']}</td>
-      <td>${members[i]['join-date']}</td>
-      <td>${members[i]['type']}</td>
-      <td>${members[i]['sub-due-month']}</td>
-    </tr>`;
-    $('#members-table tbody').append(member_row_html)
-    console.log(members[i])
+  if (members.length > 0) {
+
+    page_number = page_number || 0;
+    let member_page_groups = chunk(members, 10)
+
+    $('#members-table tbody').html('')
+    for (var i in member_page_groups[page_number]) {
+
+      let member_row_html = `<tr>
+        <td>${members[i]['member-number']}</td>
+        <td>${members[i]['first-name']}</td>
+        <td>${members[i]['last-name']}</td>
+        <td>${members[i]['address01']} <br> ${members[i]['address02']} <br> ${members[i]['address03']} </td>
+        <td>${members[i]['sex']}</td>
+        <td>${members[i]['dob']}</td>
+        <td>${members[i]['join-date']}</td>
+        <td>${members[i]['type']}</td>
+        <td>${members[i]['sub-due-month']}</td>
+      </tr>`
+      $('#members-table tbody').append(member_row_html)
+      console.log(members[i])
+    }
+
+    if (member_page_groups.length > 1) {
+      create_pagination(member_page_groups)
+    }
+  } else {
+    $('#members-table tbody').html('nothing here')
   }
 
+}
+
+function create_pagination(pages) {
+
+  $('#members-pagination').html('')
+
+  for (var i = 0; i < pages.length; i++) {
+    let item_html = `<span class="item" data-page="${i}">${i+1}</span>`
+    $('#members-pagination').append(item_html)
+  }
+  $('#members-pagination span.item').click(function() {
+    $this = $(this)
+    make_table($this.data('page'));
+  });
 }
